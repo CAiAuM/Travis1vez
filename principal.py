86de635db2b433070914e3be5382e8e63d30d837
Caio Monteiro de Figueiredo <1802232@SALAS.AULAS>
2019-03-18 21:55:46 -0300

    pode ser qualquer mensagem

---
+++ principal.py
@@ -0,0 +1,9 @@
+def soma (x,y):
+    return x+y
+
+def subtrai (x,y):
+    return x-y
+
+def mult():
+    pass
+
---
+++ teste.py
@@ -0,0 +1,6 @@
+import pytest
+from principal import soma
+from principal import subtrai
+def test_soma():
+    assert soma (2,4)==6
+
